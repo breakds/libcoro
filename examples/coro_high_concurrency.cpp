22a30823d55270b3fe94b0d51f36126645497a98
Break Yang <breakds@gmail.com>
2025-01-24 09:48:00 -0800

    Improved bug reproduce

--- examples/coro_high_concurrency.cpp
+++ examples/coro_high_concurrency.cpp
@@ -7,29 +7,21 @@
 
 static void ManySmallTask()
 {
+    int N = 1000000;
+
     auto scheduler = coro::io_scheduler::make_shared(coro::io_scheduler::options{
         .thread_strategy = coro::io_scheduler::thread_strategy_t::spawn,
-        .pool            = coro::thread_pool::options{.thread_count = 5}});
+        .pool            = coro::thread_pool::options{.thread_count = 2}});
 
     std::atomic_int32_t total = 0;
 
     auto increment = [](std::atomic_int32_t* count) -> coro::task<void>
     {
-        // NOTE(breakds): There are quite some mysterious issues regarding
-        // this behavior here.
-        //
-        // 1. Without adding the yield_for here, there is a very high possibility
-        //    that it will drop some tasks. With te `yield_for`, the possibility is
-        //    much lower.
-        //
-        // 2. With a single thread, if every tasks `yield_for` 50 seconds it would
-        //    take a very long time to finish all this. However, in this case all
-        //    1000000 tasks finished in
         ++(*count);
         co_return;
     };
 
-    for (int i = 0; i < 1000000; ++i)
+    for (int i = 0; i < N; ++i)
     {
         scheduler->schedule(increment(&total));
     }
@@ -37,6 +29,7 @@ static void ManySmallTask()
     while (true)
     {
         printf("total = %d\n", total.load());
+        scheduler->print_pool_info();
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
